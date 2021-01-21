86f22b2efdb77c01a570aa84dd9242a46a931b85
Jan Romann <jan.romann@gmail.com>
2021-01-21 21:49:28 +0100

    Add last version of the homepage

--- index.php
+++ index.php
@@ -2,15 +2,45 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="The Webpage of Every Case Games">
+
+    <link rel="stylesheet" href="style.css" />
+
+    <title>Every Case Games</title>
+
 </head>
 
 <body>
-    <h1>Hello World!</h1>
+    <div class="hero">
+        <img src="cases.jpg" alt="" class="hero__image">
+        <div class="hero__claim">
+            <div class="container">
+                <p class="hero__text">
+                    The best Gamedevs for every case.
+                </p>
+                <a href="mailto:jan.romann@gmail.com" class="hero__button">
+                    <p class="hero__button-text">
+                        Get in touch with us.
+                    </p>
+                </a>
+            </div>
+        </div>
+    </div>
 
-    <p>Dies hier ist unsere hervorragende Test-Seite!</p>
+    <section class="about-us">
+        <div class="container">
+            <h2 class="about-us__heading">
+                New to the game. But not to games.
+            </h2>
+
+            <p class="about-us__text">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium exercitationem dicta quidem ipsa enim, molestiae provident omnis sapiente aliquid ea ratione atque harum nemo veritatis explicabo! Iure illo amet qui.
+            </p>
+        </div>
+    </section>
+
 </body>
 
 </html>
